8e3c9c70c3b437f826c25816abed43fb8485db1f
Dan B <me@narry.land>
2019-06-03 20:34:53 -0400

    Disable asserts

--- pcsx2/gui/AppAssert.cpp
+++ pcsx2/gui/AppAssert.cpp
@@ -119,7 +119,7 @@ void Pcsx2App::OnAssertFailure( const wxChar *file, int line, const wxChar *func
 bool AppDoAssert( const DiagnosticOrigin& origin, const wxChar *msg )
 {
 	// Used to allow the user to suppress future assertions during this application's session.
-	static bool disableAsserts = false;
+	static bool disableAsserts = true; //Vanguard - Disable asserts
 	if( disableAsserts ) return false;
 
 	wxString trace( pxGetStackTrace(origin.function) );
