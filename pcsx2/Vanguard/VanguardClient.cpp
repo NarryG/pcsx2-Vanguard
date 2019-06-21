277441675708366a920b1a1cc78ff88ed147a675
Dan B <me@narry.land>
2019-06-20 21:18:27 -0400

    Initialize a default console

--- pcsx2/Vanguard/VanguardClient.cpp
+++ pcsx2/Vanguard/VanguardClient.cpp
@@ -90,7 +90,7 @@ static PartialSpec ^ getDefaultPartial() {
     partial->Set(VSPEC::SUPPORTS_SAVESTATES, true);
     partial->Set(VSPEC::SUPPORTS_MIXED_STOCKPILE, true);
     partial->Set(VSPEC::CONFIG_PATHS, VanguardClient::configPaths);
-    partial->Set(VSPEC::SYSTEM, String::Empty);
+    partial->Set(VSPEC::SYSTEM, "PCSX2");
     partial->Set(VSPEC::GAMENAME, String::Empty);
     partial->Set(VSPEC::SYSTEMPREFIX, String::Empty);
     partial->Set(VSPEC::OPENROMFILENAME, String::Empty);
