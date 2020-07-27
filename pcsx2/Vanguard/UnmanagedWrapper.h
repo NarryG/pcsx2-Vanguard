86e948c70ca3b787799710f77913107ee835ab8a
NarryG <me@narry.land>
2020-07-27 16:16:37 -0400

    Add missing files

---
+++ pcsx2/Vanguard/UnmanagedWrapper.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "wx/string.h"
+
+
+typedef void (*FnPtr_VanguardMethod)();
+class UnmanagedWrapper
+{
+public:
+    static void VANGUARD_EXIT();
+    static void VANGUARD_LOADSTATE(const wxString &file);
+    static void VANGUARD_SAVESTATE(const wxString &file);
+    static void VANGUARD_RESUMEEMULATION();
+    static void VANGUARD_STOPGAME();
+    static void VANGUARD_LOADGAME(const wxString &file);
+    static std::string VANGUARD_GETGAMENAME();
+    static void EERAM_POKEBYTE(long long addr, unsigned char val);
+    static unsigned char EERAM_PEEKBYTE(long long addr);
+    static std::string VANGUARD_SAVECONFIG();
+    static void VANGUARD_LOADCONFIG(wxString cfg);
+};
+
